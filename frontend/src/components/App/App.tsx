c6d817937257537014f35a130f2ebe0883232d26
harystyleseze <ceze5265@gmail.com>
2025-09-24 22:15:08 +0100

    initial commit

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+      <>
+          <div className="p-4 bg-blue-200 rounded-lg text-center">
+              <h1>Privexbot AI chatbot builder</h1>
+          </div>
+          <div className="flex min-h-svh flex-col items-center justify-center">
+              <Button>Click me</Button>
+          </div>
+      </>
+  )
+}
+
+export default App
